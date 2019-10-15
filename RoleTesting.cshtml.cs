d9d3ecf721d9d5edc137dd5710013594581d8fbf
elTigre9 <33560876+elTigre9@users.noreply.github.com>
2019-10-15 14:37:23 -0500

    Added Role test and startup file
    The role testing helps us to understand how different roles help us specify views and grab data. The startup file adds authorization code for access to only logged in users.

---
+++ RoleTesting.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetDinos.Pages
+{
+    public class RoleTestingModel : PageModel
+    {
+        public string CurrentUserRole { get; set; }
+        public void OnGet()
+        {
+            CurrentUserRole = "DH";
+        }
+    }
+}
